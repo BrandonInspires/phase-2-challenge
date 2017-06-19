b7feeeef33a91565f6abca335c97864237daa77d
Brandon Gilchrist <brandongilchrist@Brandons-MacBook-Pro.local>
2017-06-19 13:06:28 -0700

    Working with Hardcoded Data

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,3 +1,5 @@
  const clients = require('./clients.json');
- var returnstuff = clients.filter(function(item) {return item.rep_name == "Xymenes Dewer"; });
+ var name = "Xymenes Dewer";
+ var returnstuff = clients.filter(function(item) {return item.rep_name == name; });
+ console.log('Finding Clients With Name "' + name + '"' );
  console.log(returnstuff);
