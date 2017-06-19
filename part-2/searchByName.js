11b1ba8c8d637449dcda07412e86115579c1ad7a
Brandon Gilchrist <brandongilchrist@Brandons-MacBook-Pro.local>
2017-06-19 14:20:33 -0700

    hard coded linebreak

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,8 +1,9 @@
  const clients = require('./clients.json');
  var name = process.argv[2];
  var returnstuff = clients.filter(function(item) {return (item.rep_name.startsWith(name)) });
- console.log('Finding Clients With Name "' + name + '"' );
+ console.log('Finding Clients With Name "' + name + '"...' );
  returnstuff.forEach(function(element) {
+      console.log('')
       console.log("id:", element.id);
       console.log("rep_name:", element.rep_name);
  }, this);
