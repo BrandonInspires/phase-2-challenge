a3391e96417f440ee1d315c90f3788ea8f2b2c16
Brandon Gilchrist <brandongilchrist@Brandons-MacBook-Pro.local>
2017-06-19 13:39:41 -0700

    Command Line Arguments working

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,5 +1,7 @@
  const clients = require('./clients.json');
- var name = "Xymenes Dewer";
+ var name = process.argv[2];
  var returnstuff = clients.filter(function(item) {return item.rep_name == name; });
  console.log('Finding Clients With Name "' + name + '"' );
  console.log(returnstuff);
+
+console.log(returnstuff.id)
