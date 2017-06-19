4c29bc8a78382e1b211835088afc2eec608cb374
Brandon Gilchrist <brandongilchrist@Brandons-MacBook-Pro.local>
2017-06-19 14:07:58 -0700

    Using a ForEach Return

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,7 +1,9 @@
  const clients = require('./clients.json');
  var name = process.argv[2];
- var returnstuff = clients.filter(function(item) {return item.rep_name == name; });
+ var returnstuff = clients.filter(function(item) {return (item.rep_name == name) });
  console.log('Finding Clients With Name "' + name + '"' );
- console.log(returnstuff);
+ returnstuff.forEach(function(element) {
+      console.log("id:", element.id);
+ }, this);
+ 
 
-console.log(returnstuff.id)
