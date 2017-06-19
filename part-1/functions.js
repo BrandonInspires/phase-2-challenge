ac67a5445ff4aa77aabd6b27903e1fd839d1056a
Brandon Gilchrist <brandongilchrist@Brandons-MacBook-Pro.local>
2017-06-19 16:17:25 -0700

    Date function not yet working

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,5 +1,30 @@
 function weekday(date) {
-
+    switch (true) {
+        case date.getDay == 0:
+        return "Sunday";
+        break;
+        case date.getDay == 1:
+        return "Monday";
+        break;
+        case date.getDay == 2:
+        return "Tuesday";
+        break;
+        case date.getDay == 3:
+        return "Wednesday";
+        break;
+        case date.getDay == 4:
+        return "Thursday";
+        break;
+        case date.getDay == 5:
+        return "Friday";
+        break;
+        case date.getDay == 6:
+        return "Saturday";
+        break;
+        default:
+        return false;
+        break;
+    }
 }
 
 function snippet(string, maxlength) {
